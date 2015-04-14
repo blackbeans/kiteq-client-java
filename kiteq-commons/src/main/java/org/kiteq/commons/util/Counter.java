9edf2de1a551c8583f7e985432e6f33a0842aeeb
luofucong <bayinamine@gmail.com>
2015-04-14 14:45:10 +0800

    refactor

--- kiteq-commons/src/main/java/org/kiteq/commons/util/Counter.java
+++ kiteq-commons/src/main/java/org/kiteq/commons/util/Counter.java
@@ -1,52 +1,27 @@
 package org.kiteq.commons.util;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author gaofeihang
  * @since Feb 25, 2015
  */
 public class Counter {
-    
-    private AtomicLong count = new AtomicLong(0);
-    private long prevCount;
-    
-    public Counter() {}
-    
-    public Counter(int count) {
-        this.count.set(count);
-    }
+
+    private final AtomicReference<AtomicLong> counter = new AtomicReference<AtomicLong>(new AtomicLong(0));
 
     public long inc() {
-        return count.incrementAndGet();
-    }
-    
-    public long inc(long i) {
-        return count.addAndGet(i);
-    }
-    
-    public long dec() {
-        return count.decrementAndGet();
-    }
-    
-    public long dec(long i) {
-        return count.addAndGet(-i);
-    }
-    
-    public void clear() {
-        count.set(0);
-        prevCount = 0;
-    }
-    
-    public long getCount() {
-        return count.get();
+        return counter.get().incrementAndGet();
     }
     
     public long getCountChange() {
-        long tempCount = count.get();
-        long change = tempCount - prevCount;
-        prevCount = tempCount;
-        return change;
+        boolean cas;
+        AtomicLong count;
+        do {
+            count = counter.get();
+            cas = counter.compareAndSet(count, new AtomicLong(0));
+        } while (!cas);
+        return count.get();
     }
-    
 }

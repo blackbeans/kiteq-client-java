94527dfb7b08ecc7f0a77e4455a915ae1f5ba1f3
Mohamed Ezzat <mohamed.ezzat@devfactory.com>
2016-06-23 09:47:09 +0200

    squid:SwitchLastCaseIsDefaultCheck - switch statements should end with a default clause

--- kiteq-client/src/main/java/org/kiteq/client/binding/AbstractChangeWatcher.java
+++ kiteq-client/src/main/java/org/kiteq/client/binding/AbstractChangeWatcher.java
@@ -50,6 +50,8 @@ public abstract class AbstractChangeWatcher implements CuratorWatcher{
             case NodeDataChanged:
                 break;
             //ignore
+            default:
+                break;
         }
 
 
